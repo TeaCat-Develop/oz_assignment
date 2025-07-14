057d8ee4beaf9754e5a9c1f2a24edc1fe0322857
TeaCat-Develop <TeaCat0416@gmail.com>
2025-07-14 10:56:36 +0900

    day 3
    day3 파일 추가

---
+++ PYTHON/day_3/test.py
@@ -0,0 +1,14 @@
+print(3, 6, 9)
+print(f'syntaxerror=문법오류')
+print('반가워요' '여러분')
+print("반가워요 여러분")
+#mouseover
+
+print(3,6,9,sep="\n")
+print('010',234,5678,sep="-")
+#a='010',234,5678,sep="-"
+
+print('무궁화 꽃이 피었습니다.',end='')
+print(3,end='')
+print(3,end='')
+print(1)
